b1db3f557fae0a6a8a762c8859ce0e6dba7a552c
Bondarenko Igor <isida0@ukr.net>
2022-06-26 18:48:17 +0300

    firstlesson

---
+++ lesson 2/script.js
@@ -0,0 +1,28 @@
+let user = {
+    FirstName: "Igor",
+    LastName: "Bondarenko",
+    Age: 29
+};
+
+console.log(user);
+
+
+let UserName = prompt("What is your name?");
+
+let UserAges = prompt("How old are you?");
+
+console.log(UserName);
+console.log(UserAges);
+
+
+
+
+
+
+
+
+
+
+
+
+
